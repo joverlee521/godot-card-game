2a984fa097ba7261ca7a004176f234b55dc473f2
Jover Lee <joverlee521@gmail.com>
2024-04-07 19:34:47 -0700

    Add 5 hardcoded card positions

--- card_battle.gd
+++ card_battle.gd
@@ -1,6 +1,8 @@
 extends Node
 
 
+var current_position = 1
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,5 +15,10 @@ func _process(delta):
 
 func _on_deck_dealt_card(new_card):
 	add_child(new_card)
-	new_card.position = $CardPosition.position
+
+	new_card.position = get_node('CardPosition%s' % current_position).position
 	new_card.reveal_card()
+
+	current_position += 1
+	if current_position > 5:
+		current_position = 1
