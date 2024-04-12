e2406412a628461d7b568621ccbe252ee9db8375
Jover Lee <joverlee521@gmail.com>
2024-04-11 20:41:13 -0700

    Add player_hand scene
    Replaces the hard-coded position markers in the main card_battle scene.
    Fills the player_hand space with dealt cards based on the current
    set hand_size.
    
    In testing, a hand_size of 30 cards still looked reasonable!

--- scenes/card_battle.gd
+++ scenes/card_battle.gd
@@ -1,24 +1,12 @@
 extends Node
 
 
-var current_position = 1
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
-func _on_deck_dealt_card(new_card):
-	add_child(new_card)
-
-	new_card.position = get_node('CardPosition%s' % current_position).position
-	new_card.reveal_card()
-
-	current_position += 1
-	if current_position > 5:
-		current_position = 1
---
+++ scenes/player_hand.gd
@@ -0,0 +1,40 @@
+extends ColorRect
+
+signal hand_full
+
+var hand_size = 5
+var card_y
+var card_x_spacing
+var cards_in_hand = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var rect = get_rect()
+	var position = rect.position
+	var size = rect.size
+
+	card_y = size.y/2
+	card_x_spacing = size.x/(hand_size + 1)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_deck_dealt_card(new_card):
+	if cards_in_hand.size() >= hand_size:
+		print("MAX HAND SIZE")
+		return
+
+	cards_in_hand.append({
+		'card_name': new_card.card_name,
+		'card_suit': new_card.card_suit,
+	})
+
+	add_child(new_card)
+	#new_card.set_owner(self)
+
+	var card_x = cards_in_hand.size() * card_x_spacing
+	new_card.position = Vector2(card_x, card_y)
+	new_card.reveal_card()
