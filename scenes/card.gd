7b53d54d9a2c9430258cbf301783f3769431c552
Jover Lee <joverlee521@gmail.com>
2024-04-13 13:34:28 -0700

    card_scene: Add custom input handler for clicking
    Using custom input handler to work around issue where overlapping
    Area2D objects both get input events.
    
    Can remove workaround if upgrading to Godot v4.3.

--- scenes/card.gd
+++ scenes/card.gd
@@ -23,6 +23,8 @@ const card_values = {
 var card_suit: String
 var card_name: String
 var card_value: int
+
+var mouse_entered_card: bool = false
 var card_selected: bool = false
 
 
@@ -49,13 +51,24 @@ func reveal_card():
 	$Sprite2D.texture = load(image_file)
 
 
-func _on_input_event(viewport, event, shape_idx):
-	if (event.is_pressed()
-		and event.button_index == MOUSE_BUTTON_LEFT):
-
+# Custom handler for input to work around overlapping Area2D objects both getting input
+# See https://github.com/godotengine/godot/issues/29825
+# Resolved in https://github.com/godotengine/godot/pull/75688
+# which was released in Godot v4.3
+func _unhandled_input(event):
+	if (event.is_action_pressed("mouse_left_click")
+	and mouse_entered_card):
 		card_selected = !card_selected
 
 		if card_selected:
 			position.y = position.y - 50
 		else:
 			position.y = position.y + 50
+
+		self.get_viewport().set_input_as_handled()
+
+func _on_Area2D_mouse_entered():
+	mouse_entered_card = true
+
+func _on_Area2D_mouse_exited():
+	mouse_entered_card = false
